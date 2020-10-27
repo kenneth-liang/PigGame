4e7b267873a23c318ba25916cd4a51acd4e5e9b2
Kenneth <kennethkjliang@gmail.com>
2020-10-26 22:39:21 -0700

    updated game

--- game.js
+++ game.js
@@ -18,12 +18,15 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     document.getElementById('dice-2').style.display = 'block';
     document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
     document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+    document.getElementById("switch").style = "visibility: hidden";
     //3. Update the round score IF the rolled number was NOT a 1
     if (dice1 !== 1 && dice2 !== 1) {
+      
       //Add score
       roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
+      document.getElementById("switch").style = "visibility: visible";
       //Next player
       nextPlayer();
     }
@@ -84,8 +87,8 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active');
   //document.querySelector('.player-0-panel').classList.remove('active');
   //document.querySelector('.player-1-panel').classList.add('active');
-  document.getElementById('dice-1').style.display = 'none';
-  document.getElementById('dice-2').style.display = 'none';
+  // document.getElementById('dice-1').style.display = 'none';
+  // document.getElementById('dice-2').style.display = 'none';
 }
 document.querySelector('.btn-new').addEventListener('click', init);
 function init() {
